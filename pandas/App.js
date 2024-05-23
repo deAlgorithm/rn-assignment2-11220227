10476cbac23eb852595c163f00b51fbc221ec4ea
deAlgorithm <iotabil@st.ug.edu.gh>
2024-05-23 22:22:51 +0000

    Increase the font size of the text to 24

--- pandas/App.js
+++ pandas/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My name is Otabil</Text>
+      <Text style={styles.name}>My name is Otabil</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +17,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  name :{
+    fontSize : 24,
+  }
 });
